0f7a6c6096d29f93ed6172b63d1bb3b8044ce432
whchoi98 <whchoi98@gmail.com>
2022-11-07 23:01:11 +0900

    create new file - ec2_public_ip.sh

--- ec2_public_ip.sh
+++ ec2_public_ip.sh
@@ -17,12 +17,12 @@ export Public_12_102_IP=$(aws ec2 describe-instances --filters 'Name=tag:Name,Va
 export Public_12_103_IP=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=ICNVPC-Public-10.11.12.103' | jq -r '.Reservations[].Instances[].PublicIpAddress')
 export Public_12_104_IP=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=ICNVPC-Public-10.11.12.104' | jq -r '.Reservations[].Instances[].PublicIpAddress')
 echo "export Public_11_101_IP=${Public_11_101_IP}"| tee -a ~/.bash_profile
-echo "export Public_11_102_IP=${Public_11_101_IP}"| tee -a ~/.bash_profile
-echo "export Public_11_103_IP=${Public_11_101_IP}"| tee -a ~/.bash_profile
-echo "export Public_11_104_IP=${Public_11_101_IP}"| tee -a ~/.bash_profile
-echo "export Public_11_101_IP=${Public_11_101_IP}"| tee -a ~/.bash_profile
-echo "export Public_11_102_IP=${Public_11_101_IP}"| tee -a ~/.bash_profile
-echo "export Public_11_103_IP=${Public_11_101_IP}"| tee -a ~/.bash_profile
-echo "export Public_11_104_IP=${Public_11_101_IP}"| tee -a ~/.bash_profile
+echo "export Public_11_102_IP=${Public_11_102_IP}"| tee -a ~/.bash_profile
+echo "export Public_11_103_IP=${Public_11_103_IP}"| tee -a ~/.bash_profile
+echo "export Public_11_104_IP=${Public_11_104_IP}"| tee -a ~/.bash_profile
+echo "export Public_12_101_IP=${Public_12_101_IP}"| tee -a ~/.bash_profile
+echo "export Public_12_102_IP=${Public_12_102_IP}"| tee -a ~/.bash_profile
+echo "export Public_12_103_IP=${Public_12_103_IP}"| tee -a ~/.bash_profile
+echo "export Public_12_104_IP=${Public_12_104_IP}"| tee -a ~/.bash_profile
 source ~/.bash_profile
 
