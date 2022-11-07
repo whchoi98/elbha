8a3c0438c2f85e1470ec9e6704ee1b1f621dea31
whchoi98 <whchoi98@gmail.com>
2022-11-07 22:31:53 +0900

    create new file - ELB_URL_export.sh

--- ELB_URL_export.sh
+++ ELB_URL_export.sh
@@ -2,20 +2,20 @@
 # ELB URL export 
 ## ELB DNS Name
 aws elbv2 describe-load-balancers --names ICNVPC-InternetALB1 | jq -r '.LoadBalancers[].DNSName'
-export ICNVPC-InternetALB1-URL=$(aws elbv2 describe-load-balancers --names ICNVPC-InternetALB1 | jq -r '.LoadBalancers[].DNSName')
-echo "export ICNVPC-InternetALB1-URL=${ICNVPC-InternetALB1-URL}"| tee -a ~/.bash_profile
+export ICNVPC_InternetALB1_URL=$(aws elbv2 describe-load-balancers --names ICNVPC-InternetALB1 | jq -r '.LoadBalancers[].DNSName')
+echo "export ICNVPC_InternetALB1_URL=${ICNVPC_InternetALB1_URL}"| tee -a ~/.bash_profile
 
 aws elbv2 describe-load-balancers --names ICNVPC-InternetALB2 | jq -r '.LoadBalancers[].DNSName'
-export ICNVPC-InternetALB2-URL=$(aws elbv2 describe-load-balancers --names ICNVPC-InternetALB2 | jq -r '.LoadBalancers[].DNSName')
-echo "export ICNVPC-InternetALB2-URL=${ICNVPC-InternetALB2-URL}"| tee -a ~/.bash_profile
+export ICNVPC_InternetALB2_URL=$(aws elbv2 describe-load-balancers --names ICNVPC-InternetALB2 | jq -r '.LoadBalancers[].DNSName')
+echo "export ICNVPC_InternetALB2_URL=${ICNVPC_InternetALB2_URL}"| tee -a ~/.bash_profile
 
 aws elbv2 describe-load-balancers --names ICNVPC-InternetNLB1 | jq -r '.LoadBalancers[].DNSName'
-export ICNVPC-InternetNLB1-URL=$(aws elbv2 describe-load-balancers --names ICNVPC-InternetNLB1 | jq -r '.LoadBalancers[].DNSName')
-echo "export ICNVPC-InternetNLB1-URL=${ICNVPC-InternetALB1-URL}"| tee -a ~/.bash_profile
+export ICNVPC_InternetNLB1_URL=$(aws elbv2 describe-load-balancers --names ICNVPC-InternetNLB1 | jq -r '.LoadBalancers[].DNSName')
+echo "export ICNVPC_InternetNLB1_URL=${ICNVPC_InternetALB1_URL}"| tee -a ~/.bash_profile
 
 aws elbv2 describe-load-balancers --names ICNVPC-InternetNLB2 | jq -r '.LoadBalancers[].DNSName'
-export ICNVPC-InternetNLB2-URL=$(aws elbv2 describe-load-balancers --names ICNVPC-InternetNLB2 | jq -r '.LoadBalancers[].DNSName')
-echo "export ICNVPC-InternetNLB2-URL=${ICNVPC-InternetNLB2-URL}"| tee -a ~/.bash_profile
+export ICNVPC_InternetNLB2_URL=$(aws elbv2 describe-load-balancers --names ICNVPC-InternetNLB2 | jq -r '.LoadBalancers[].DNSName')
+echo "export ICNVPC_InternetNLB2_URL=${ICNVPC_InternetNLB2_URL}"| tee -a ~/.bash_profile
 
 source ~/.bash_profile
 
